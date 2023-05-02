413ed9092456df9cf78669df036dc48a2391a115
dmytro-chushko <dmytro.chushko@gmail.com>
2023-05-02 18:43:55 +0300

    feat: Add from ono to n if n < 0

--- from-one-to-n.js
+++ from-one-to-n.js
@@ -1,11 +1,11 @@
 // Return digits from 1 to n
 
 const digitsFromOneTo = (n) => {
-  const absN = Math.abs(n);
-  const arr = [absN];
+  // const absN = Math.abs(n);
+  const arr = [n];
 
-  for (let i = absN; i > 1; i--) {
-    arr.push(i - 1);
+  for (let i = n; i !== 1; n > 0 ? i-- : i++) {
+    arr.push(n > 0 ? i - 1 : i + 1);
   }
 
   return arr.join(", ");
