ed518b6d532a802b7a61c3b49c55f66bbf3c8135
dmytro-chushko <dmytro.chushko@gmail.com>
2023-06-01 13:53:51 +0300

    fix: Refactor the flatArray function

--- to-flat-the-array.js
+++ to-flat-the-array.js
@@ -2,16 +2,23 @@
 
 const flatArray = (arr) => {
   if (!Array.isArray(arr)) return arr;
-  const internalArray = arr.reduce((newArr, el) => {
-    // if (Array.isArray(el)) {
-    //   console.log(Array.isArray(el));
-    return newArr.concat(flatArray(el));
-    // } else {
-    //   return newArr.push(el);
-    // }
-  }, []);
 
-  return [...internalArray];
+  return arr.reduce((newArr, el) => {
+    return newArr.concat(flatArray(el));
+  }, []);
 };
 
-console.log(flatArray([[1, 2], [[3, 4, 5]], [[[6, 7, 8, 9]]]]));
+console.log(
+  flatArray([
+    [1, 2],
+    [[3, 4, 5]],
+    [
+      [
+        [
+          [6, 7],
+          [8, 9],
+        ],
+      ],
+    ],
+  ])
+);
