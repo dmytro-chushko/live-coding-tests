0642cfd5bfd4be6d96034708420f4fb8e3e27948
dmytro-chushko <dmytro.chushko@gmail.com>
2024-01-31 23:04:43 +0200

    Add comment

--- setTImout_in_cycle.js
+++ setTImout_in_cycle.js
@@ -1,16 +1,16 @@
-for (var i = 1; i <= 5; i++) {
-  setTimeout(function timer() {
-    console.log(i);
-  }, i * 1000);
-}
+// for (var i = 1; i <= 5; i++) {
+//   setTimeout(function timer() {
+//     console.log(i);
+//   }, i * 1000);
+// }
 
-for (var i = 1; i <= 5; i++) {
-  (function (j) {
-    setTimeout(function timer() {
-      console.log(j);
-    }, j * 1000);
-  })(i);
-}
+// for (var i = 1; i <= 5; i++) {
+//   (function (j) {
+//     setTimeout(function timer() {
+//       console.log(j);
+//     }, j * 1000);
+//   })(i);
+// }
 
 for (let i = 1; i <= 5; i++) {
   setTimeout(function timer() {
